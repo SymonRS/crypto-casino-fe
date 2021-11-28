e41d044e5cf1d602e0b306c0751a962e136ad9ab
Simone Ventura <simone.ventura@soprasteria.com>
2021-11-28 11:15:57 +0100

    Deleted unused files and added routing support

---
+++ src/routing/routerConfig.tsx
@@ -0,0 +1,27 @@
+import {
+    BrowserRouter as Router,
+    Routes as Switch,
+    Route
+} from "react-router-dom";
+import { IRoute } from "./types";
+
+const routes: IRoute[] = [
+    /* {
+      path: "/sandwiches",
+      component: Sandwiches
+    },
+    {
+      path: "/tacos",
+      component: Tacos,
+      routes: [
+        {
+          path: "/tacos/bus",
+          component: Bus
+        },
+        {
+          path: "/tacos/cart",
+          component: Cart
+        }
+      ]
+    } */
+  ];
