bda52f39547f5355bacbbbcf376c64229ee7ba13
Simone Ventura <simone.ventura@soprasteria.com>
2022-03-12 11:58:25 +0100

    Form update

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
 import {Button, Card, Form, Spinner} from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
@@ -7,47 +7,71 @@ import { isLoadingAtom } from "../../atoms/application/application";
 import { userInfoAtom } from "../../atoms/user/user";
 import { globalToastTheme } from "../../config/toast.config";
 import {doSignup} from "../../restAPI/userAuthAPIs";
+import { useFormik, Formik } from "formik";
+import * as Yup from "yup";
+import styled, { DefaultTheme, StyledComponent } from "styled-components";
+
+const FormInitValues = () => {
+    return {
+        email: '',
+        username: '',
+        password: '',
+        confPassword: ''
+    }
+};
+
 
 const SignupForm: () => JSX.Element = () => {
+
+    const { t } = useTranslation();
+
+    const customSchema = Yup.object({
+        username: Yup.string().min(4).max(20, t('form.error.usernameMaxLength')).required(t('form.error.usernameRequired')),
+        email: Yup.string().email(t('form.error.invalidMail')).required(t('form.error.emailRequired')).min(5, t('form.error.emailMinLength')).max(60, t('form.error.emailMaxLength')),
+        password: Yup.string().min(8, t('form.error.passMinLength')).max(20, t('form.error.passMaxLength')).required(t('form.error.passwordRequired')).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, t('form.error.passNoRequirements')),
+        confPassword: Yup.string().oneOf([Yup.ref('password'), null], t('form.error.noPassMatch')).required(t('form.error.confPassRequired'))
+    })
 
     const [isLoading, setIsLoading] = useRecoilState(isLoadingAtom);
     const [, setUserInfo] = useRecoilState(userInfoAtom);
 
-    const { t } = useTranslation();
-
-    const [isValidForm, setIsValidForm] = useState(false);
-
-    const [formState, setFormState]: [ISignupForm, Dispatch<SetStateAction<ISignupForm>>] = useState({
+    /* const [formState, setFormState]: [ISignupForm, Dispatch<SetStateAction<ISignupForm>>] = useState({
         email: '',
         username: '',
         password: '',
         confPass: ''
-    });
+    }); */
 
-    const handleInput: (event: ChangeEvent<any>) => void = (event: ChangeEvent<any>) => {
+    /* const handleInput: (event: ChangeEvent<any>) => void = (event: ChangeEvent<any>) => {
         let newState: ISignupForm = {
             ...formState,
             [event.currentTarget.name]: event.currentTarget.value
         };
 
         setFormState(newState);
-    }
+    } */
 
-    useEffect(() => {
+   /*  useEffect(() => {
         validateInput()
-    }, [formState])
+    }, [formState]) */
 
-    const validateInput = () => {
+    /* const validateInput = () => {
         if(formState.username?.length <= 0) return setIsValidForm(false);
         if(formState.email?.length <= 0) return setIsValidForm(false);
         if(formState.password.length <= 0) return setIsValidForm(false);
         if(formState.password !== formState.confPass) return setIsValidForm(false);
 
         setIsValidForm(true);
-    };
+    }; */
 
-    const handleSingup = () => {
-        setIsLoading(true);
+    useEffect(() => {
+
+    })
+
+
+    const handleSignup = (object: any) => {
+        console.log('Form obj: ', object);
+        /* setIsLoading(true);
         doSignup(formState)
             .then(userInfo => {
                     setIsLoading(false);
@@ -59,41 +83,76 @@ const SignupForm: () => JSX.Element = () => {
                     setIsLoading(false);
                     toast(t('toast.registration.error'), {type: 'error', theme: globalToastTheme})
                 }
-            )
+            ) */
     }
 
     return(
         <Card>
             <Card.Body>
                 {isLoading && <Spinner className="loading-spinner" animation="grow"/>}
-                <Form as={'div'} >
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control name={'email'} value={formState.email} onChange={e => handleInput(e)} type="email" placeholder={t('form.email')} />
-                        <Form.Text className="text-muted">
-                            {t('form.nonDisclousureMessage')}
-                        </Form.Text>
-                    </Form.Group>
+                <Formik 
+                initialValues={FormInitValues}
+                validationSchema={customSchema}
+                onSubmit={e => handleSignup(e)}
+                >
+                {({
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    touched,
+                    isValid,
+                    errors,
+                    }) => (
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" >
+                                <Form.Control required id='email' name='email' isValid={touched['email'] && !errors['email']} value={values['email']} onChange={handleChange} type="email" placeholder={t('form.email')} />
+                                <Form.Text className="text-muted">
+                                    {t('form.nonDisclousureMessage')}
+                                </Form.Text>
+                                <Form.Control.Feedback>{t('form.validationOkMessage')}</Form.Control.Feedback>
+                                {errors['email']? <FormError>{errors['email']}</FormError> : ''}
+                            </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicUsername">
-                        <Form.Control name={'username'} value={formState.username} onChange={e => handleInput(e)} type="text" placeholder={t('form.username')} />
-                    </Form.Group>
+                            {console.log(errors)}
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control name={'password'} value={formState.password} onChange={e => handleInput(e)} type="password" placeholder={t('form.password')}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-                        <Form.Control name={'confPass'} value={formState.confPass} onChange={e => handleInput(e)} type="password" placeholder={t('form.confPassword')} />
-                    </Form.Group>
-                    <div className="d-flex justify-content-center align-items-center">
-                        <Button variant="primary" disabled={!isValidForm} onClick={e => handleSingup()} type="submit">
-                            {t('form.signup')}
-                        </Button>
-                    </div>
-                </Form>
+                            <Form.Group className="mb-3" >
+                                <Form.Control required id='username' name='username' isValid={touched['username'] && !errors['username']} value={values['username']} onChange={handleChange} type="text" placeholder={t('form.username')} />
+                                <Form.Control.Feedback>{t('form.validationOkMessage')}</Form.Control.Feedback>
+                                {errors['username'] ? <FormError>{errors['username']}</FormError> : ''}
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" >
+                                <Form.Control required aria-describedby="passwordHelpBlock" id='password' name='password' isValid={touched['password'] && !errors['password']} value={values['password']} onChange={handleChange} type="password" placeholder={t('form.password')}/>
+                                <Form.Text id="passwordHelpBlock" muted>
+                                    {t('form.passRequirements')}
+                                </Form.Text>
+                                <Form.Control.Feedback>{t('form.validationOkMessage')}</Form.Control.Feedback>
+                                {errors['password'] ? <FormError>{errors['password']}</FormError> : ''}
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" >
+                                <Form.Control  required id='confPassword' name='confPassword' isValid={touched['confPassword'] && !errors['confPassword']} value={values['confPassword']} onChange={handleChange} type="password" placeholder={t('form.confPassword')} />
+                                <Form.Control.Feedback>{t('form.validationOkMessage')}</Form.Control.Feedback>
+                                {errors['confPassword'] ? <FormError>{errors['confPassword']}</FormError> : ''}
+                            </Form.Group>
+
+                            <div className="d-flex justify-content-center align-items-center">
+                                <Button variant={'primary'}   type="submit">
+                                    {t('form.signup')}
+                                </Button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </Card.Body>
         </Card>
     )
 }
+
+const FormError: StyledComponent<"div", DefaultTheme> = styled.div`
+color: red;
+`;
 
 export interface ISignupForm{
     email: string,
